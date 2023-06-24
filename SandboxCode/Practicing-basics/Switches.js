846776fa441672fd1c0cc5e37840c3b2c6b9a992
Kiht <gabe.klinefelter@gmail.com>
2023-06-23 23:34:22 -0500

    Fixed weird prompt bug
    \n seems to work weird with prompt sync

--- SandboxCode/Practicing-basics/Switches.js
+++ SandboxCode/Practicing-basics/Switches.js
@@ -1,24 +1,24 @@
 const prompt = require("prompt-sync")({ sigint: true });
 generateResponse();
 function generateResponse() {
-	let promptAnswer = prompt("What class did you start as?\n").toUpperCase();
+	let promptAnswer = prompt("What class did you start as? ").toUpperCase();
 	switch (promptAnswer) {
 		case "ACN":
 		case "LNC":
 		case "ARC":
 		case "PUG":
 		case "THM":
-			console.log("That's a class devoted to dealing damage!\n");
+			console.log("That's a class devoted to dealing damage!");
 			break;
 		case "CNJ":
-			console.log("That's a class devoted to healing!\n");
+			console.log("That's a class devoted to healing!");
 			break;
 		case "GLD":
 		case "MRD":
-			console.log("That's a class devoted to being stepped on!\n");
+			console.log("That's a class devoted to being stepped on!");
 			break;
 		default:
-			let yesNo = prompt("I'm not familiar with that one, care to try again? Y/N\n").toUpperCase();
+			let yesNo = prompt("I'm not familiar with that one, care to try again? Y/N ").toUpperCase();
 			if (yesNo == "Y") {
 				generateResponse();
 			}
