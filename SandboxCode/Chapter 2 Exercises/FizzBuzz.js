9a2e239b42c8a5f6d9121534edc9a21b6e32cb85
Kiht <gabe.klinefelter@gmail.com>
2023-06-24 03:03:35 -0500

    FizzBuzz Cleanup
    Cleaned up FizzBuzz Code, added in initial prompt for Chessboard

--- SandboxCode/Chapter 2 Exercises/FizzBuzz.js
+++ SandboxCode/Chapter 2 Exercises/FizzBuzz.js
@@ -21,7 +21,7 @@
 //When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5
 //(and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
-for (let currentNumber = 0; currentNumber < 100; currentNumber++)
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++)
 {
 	if (currentNumber % 3 == 0 && currentNumber % 5 == 0)
 	{
