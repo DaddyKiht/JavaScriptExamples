c99dbd2e457a7399d4286ce4c45355807dcc1115
Kiht <gabe.klinefelter@gmail.com>
2023-06-24 21:10:14 -0500

    Chessboard Exercise Solution

--- SandboxCode/Chapter 2 Exercises/Chessboard.js
+++ SandboxCode/Chapter 2 Exercises/Chessboard.js
@@ -15,3 +15,18 @@
 //When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size,
 //	outputting a grid of the given width and height.
 
+let boardSize = 8;
+let boardString = "";
+
+for (let currentRow = 1; currentRow <= boardSize; currentRow++) {
+	for (let currentCol = 1; currentCol <= boardSize; currentCol++) {
+		if ((currentRow + currentCol) % 2 != 0) {
+			boardString += "#";
+		}
+		else {
+			boardString += " ";
+		}
+	}
+	boardString += `\n`;
+}
+console.log(boardString);
